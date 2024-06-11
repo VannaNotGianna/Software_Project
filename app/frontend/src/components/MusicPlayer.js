72511aedddb6617c8b7e71262b140a41051444df
victor.giroud <victor.giroud@epita.fr>
2024-06-10 21:56:36 -0500

    alias

---
+++ app/frontend/src/components/MusicPlayer.js
@@ -0,0 +1,12 @@
+// src/components/MusicPlayer.js
+import React from 'react';
+
+const MusicPlayer = () => {
+  return (
+    <div className="music-player">
+      {/* Contenu du lecteur de musique */}
+    </div>
+  );
+};
+
+export default MusicPlayer;
